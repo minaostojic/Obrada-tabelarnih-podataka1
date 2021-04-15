6057b1b1fbeb95d8ec8e5c0ef9414f96a64f0801
𝕸𝖎𝖓𝖆  𝕸 <>
2021-04-15 13:15:59 +0000

    Svi podaci u matrici

--- main.cs
+++ main.cs
@@ -2,9 +2,31 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    test123
-    proba
-    uzt
+      StreamReader podaci = new StreamReader("ulaz.txt");
+    string[,] matrica = new string[1000,6];
+    int brojac=0;
+    string s = podaci.ReadLine();
+    if (File.Exists("ulaz.txt"))
+    {
+      while (!podaci.EndOfStream)
+      {
+        s = podaci.ReadLine();
+        string[] elementi = s.Split(";"); //provera
+        for (int i=0; i<6; i++)
+        {
+          matrica[brojac,i] = elementi[i];
+        }
+        brojac++;
+      }
+    }
+    NajmanjePopularanZanr(matrica);
+    /*for (int i=0;i<1000;i++)
+    {
+      for (int j=0;j<6;j++)
+      {
+        Console.Write(matrica[i,j]+" ");
+      }
+      Console.WriteLine();
+    }*/
   }
 }
